63ca5baec91a14d9b1b741061a5665ec0a44cc4b
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-04-18 00:29:54 +0530

    Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,19 @@
+// export const environment = {
+//   production: false,
+//   baseUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
+//   frontendUrl: 'https://quantity-measurement-app-frontend-vert.vercel.app',
+//   authServiceUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
+//   historyServiceUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
+//   quantityServiceUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
+//   eurekaServiceUrl: 'https://eureka-service-production-5bd5.up.railway.app'
+// };
+
 export const environment = {
   production: false,
-  baseUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
-  frontendUrl: 'https://quantity-measurement-app-frontend-vert.vercel.app',
-  authServiceUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
-  historyServiceUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
-  quantityServiceUrl: 'https://quantitymeasurementapp-2f9c.onrender.com',
-  eurekaServiceUrl: 'https://eureka-service-production-5bd5.up.railway.app'
+  baseUrl: 'http://localhost:8080',
+  frontendUrl: 'http://localhost:4200',
+  authServiceUrl: 'http://localhost:8082',
+  historyServiceUrl: 'http://localhost:8083',
+  quantityServiceUrl: 'http://localhost:8081',
+  eurekaServiceUrl: 'http://localhost:8761'
 };
